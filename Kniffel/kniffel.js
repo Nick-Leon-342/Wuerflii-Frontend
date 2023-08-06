ab8abb52d483068eca09949fd1f1386c9e457497
nick <nick.schneider@mmtn-schneider.com>
2023-08-06 18:23:03 +0200

    Added Kniffel pics

--- Kniffel/kniffel.js
+++ Kniffel/kniffel.js
@@ -1,6 +1,6 @@
 
 
-let players;
+let players = [];
 let columns;
 const sessionStorageSubstring = "kniffelSession_";
 const upperTableID = "upperTable";
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if(sessionStorage.getItem(sessionStorageSubstring + "players")) {
 
-        players = sessionStorage.getItem(sessionStorageSubstring + "players");
+        players = JSON.parse(sessionStorage.getItem(sessionStorageSubstring + "players"));
         columns = sessionStorage.getItem(sessionStorageSubstring + "columns");
 
         createTables();
@@ -40,18 +40,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //__________________________________________________After player and column input__________________________________________________
 
-function play() {
-    players = document.getElementById("players").value;
+function next() {
+
+    let players = document.getElementById("players").value;
     columns = document.getElementById("columns").value;
 
     if(isNaN(players) || isNaN(columns) || players == 0 || columns == 0) {return}
-    
+
+    for(let i = 0; players > i; i++){addEnterNamesAndSelectColorElement(i);}
     document.getElementById("enterPlayerAndColumnCountInterface").style.display = "none";
+    document.getElementById("enterNamesInterface").style.display = "block";
+
+}
+
+function play() {
+    
+    const enterNamesElement = document.getElementsByClassName("enterNamesElement");
+    for(const element of enterNamesElement) {
+        const name = element.querySelector(".input").value;
+        const color = element.querySelector(".colorbox").value;
+        const player = {name: name, color: color};
+        players.push(player);
+
+    }
+
+    console.log(players);
+
+    deleteSessionStorage();
+
+    document.getElementById("enterNamesInterface").style.display = "none";
     document.getElementById("kniffelInterface").style.display = "block";
 
     createTables();
 
-    sessionStorage.setItem(sessionStorageSubstring + "players", players);
+    sessionStorage.setItem(sessionStorageSubstring + "players", JSON.stringify(players));
     sessionStorage.setItem(sessionStorageSubstring + "columns", columns);
     initSessionStorageTables();
     playerTable = document.getElementById(playerTable);
@@ -59,10 +81,35 @@ function play() {
 }
 
 function createTables() {
-    for(let p = 0; p < players; p++) {for(let c = 0; c < columns; c++) {
-        addColumnToTable(c + p * columns, upperTableID, p % 2 == 0 ? "lightblue" : "white");
-        addColumnToTable(c + p * columns, bottomTableID, p % 2 == 0 ? "lightblue" : "white");
-    }}
+
+    for(let p = 0; p < players.length; p++) {
+        for(let c = 0; c < columns; c++) {
+            addColumnToTable(c + p * columns, upperTableID, players[p].color);
+            addColumnToTable(c + p * columns, bottomTableID, players[p].color);
+        }
+        addColumnToPlayerTable(p);
+    }
+
+}
+
+function addEnterNamesAndSelectColorElement(i) {
+
+    const enterNamesList = document.getElementById("enterNamesList");
+    const dt = document.createElement("dt");
+    const nameInput = document.createElement("input");
+    const colorInput = document.createElement("input");
+
+    dt.classList.add("enterNamesElement");
+    nameInput.classList.add("input");
+    nameInput.value = `Player_${i+1}`
+    colorInput.classList.add("colorbox");
+    colorInput.type = "color";
+    colorInput.value = i % 2 == 0 ? "#ffffff" : "#ADD8E6";
+
+    dt.appendChild(nameInput);
+    dt.appendChild(colorInput);
+    enterNamesList.appendChild(dt);
+
 }
 
 
@@ -71,16 +118,44 @@ function createTables() {
 
 //__________________________________________________For generating Kniffel table__________________________________________________
 
+function addColumnToPlayerTable(i) {
+
+    const playerTable = document.getElementById(playerTableID);
+    const width = columns * upperTable.rows[0].cells[2].offsetWidth;
+
+    const nameTD = document.createElement("td");
+    const nameTDLabel = document.createElement("label");
+    nameTD.classList.add("playerSumElement");
+    nameTD.textContent = players[i].name;
+    nameTD.style.color = players[i].color;
+    nameTD.style.width = width + "px";
+    nameTD.appendChild(nameTDLabel);
+    playerTable.querySelectorAll("tr")[0].appendChild(nameTD);
+
+    const sumTD = document.createElement("td");
+    const sumTDLabel = document.createElement("label");
+    sumTD.classList.add("playerSumElement");
+    sumTD.style.color = players[i].color;
+    sumTD.style.width = width + "px";
+    sumTD.appendChild(sumTDLabel);
+    playerTable.querySelectorAll("tr")[1].appendChild(sumTD);
+
+
+}
+
 function addColumnToTable(column, tableID, color) {
+
     const table = document.getElementById(tableID);
     const rows = table.querySelectorAll('tr');
   
     for (let i = 0; i < rows.length - 3; i++) {addRowToTable(column, tableID, rows, true, color, i);}
     for(let i = rows.length - 3; i < rows.length; i++) {addRowToTable(column, tableID, rows, false, color, i);}
+
 }
 
 function addRowToTable(column, tableID, rows, input, color, i) {
-    const td = document.createElement('td');
+
+    const td = document.createElement("td");
     let element;
 
     if(Boolean(input)) {element = document.createElement("input");
@@ -88,7 +163,6 @@ function addRowToTable(column, tableID, rows, input, color, i) {
     element.classList.add("kniffelInput");
     element.type = "text";
     element.inputMode = "numeric";
-    element.pattern = "[1-9][0-9]*";
     element.style.backgroundColor = color;
     element.setAttribute("data-tableid", tableID);
     element.setAttribute("data-column", column);
@@ -97,14 +171,16 @@ function addRowToTable(column, tableID, rows, input, color, i) {
     td.style.backgroundColor = color;
     td.appendChild(element);
     rows[i].appendChild(td);
+
 }
 
 function inputEvent(element) {
+
     const tableID = element.getAttribute("data-tableid");
     const column = Number(element.getAttribute("data-column"));
     const row = Number(element.getAttribute("data-row"));
     const inputs = (tableID == upperTableID ? upperTable : bottomTable).querySelectorAll("input");
-    const placol = players * columns;
+    const placol = players.length * columns;
 
     saveElement(tableID, element.value, column, row);
 
@@ -126,27 +202,15 @@ function inputEvent(element) {
 
 function finishGame() {
 
-    const row = document.getElementById("playerSum");
-    const labels = bottomTable.querySelectorAll("label");
-    const playerSumWidth = columns * upperTable.rows[0].cells[2].offsetWidth;
-    const placol = players * columns;
+    const playerTableLabels = document.getElementById(playerTableID).querySelectorAll("label");
+    const placol = players.length * columns;
+    const bottomTableLabels = bottomTable.querySelectorAll("label");
+    
+    for(let i = 0; players.length > i; i++) {
 
-    for(let i = 0; players > i; i++) {
-
-        const playerSum = document.createElement("td");
-        const playerSumLabel = document.createElement("label");
-        playerSum.classList.add("playerSumElement");
-        playerSum.appendChild(playerSumLabel);
         let sum = 0;
-
-        for(let c = 0; columns > c; c++) {
-            sum += Number(labels[c + columns * i + placol * 2].textContent);
-        }
-
-        playerSumLabel.textContent = sum;
-        playerSum.style.width = playerSumWidth + "px";
-
-        row.appendChild(playerSum);
+        for(let c = 0; columns > c; c++) {sum += Number(bottomTableLabels[c + columns * i + placol * 2].textContent);}
+        playerTableLabels[i * 2 + 1].textContent = sum;
 
     }
 
@@ -157,7 +221,7 @@ function calculateColumn(upper, column) {
     const inputs = (Boolean(upper) ? upperTable : bottomTable).querySelectorAll("input");
     const upperLabels = upperTable.querySelectorAll("label");
     const bottomLabels = bottomTable.querySelectorAll("label");
-    const placol = players * columns;
+    const placol = players.length * columns;
     let sum = 0;
     
     for(let i = 0; inputs.length/placol > i; i++) {
@@ -200,7 +264,7 @@ function calculateBottomColumn(inputs, bottomLabels, column, placol, sum) {
 
 function initSessionStorageTables() {
 
-    for(let c = 0; columns * players > c; c++) {for(let r = 0; 6 > r; r++) {
+    for(let c = 0; columns * players.length > c; c++) {for(let r = 0; 6 > r; r++) {
         sessionStorage.setItem(sessionStorageSubstring + upperTableID + "_" + r + "." + c, "");
     }}
 
@@ -224,7 +288,7 @@ function loadTables() {
 }
 
 function loadTablesHelp(inputs, ID) {
-    const placol = columns * players;
+    const placol = columns * players.length;
     for(let i = 0; inputs.length > i; i++) {
         inputs[i].value = sessionStorage.getItem(sessionStorageSubstring + ID + "_" + ~~(i/placol) + "." + i%placol);
         inputEvent(inputs[i]);
