7a40460f688496235ff315656573474469923e93
nick <nick.schneider@mmtn-schneider.com>
2023-09-18 23:06:57 +0200

    Installed express and bcrypt. Added basic login system

--- server.js
+++ server.js
@@ -1,5 +1,95 @@
 
 
+const express = require("express")
+const app = express()
+const bcrypt = require("bcrypt")
+
+app.use(express.json())
+
+
+const users = []
+
+
+app.get("/users", (req, res) => {
+    res.json(users)
+})
+
+
+app.post("/users", async (req, res) => {
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = {Name: req.body.name, Password: hashedPassword}
+        users.push(user)
+        
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
+
+})
+
+
+app.post("/users/login", async (req, res) => {
+    const user = users.find(user => user.Name = req.body.Name)
+    if(user == null) {return res.status(400).send("Wrong credentials")}
+
+    try {
+        if(await bcrypt.compare(req.body.password, user.Password)) {
+            res.send("Success")
+        } else {
+            return res.status(400).send("Wrong credentials")
+        }
+    } catch {
+        res.status(500).send()
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let list = [];
 
 
@@ -212,6 +302,10 @@ function getUser(name, token) {
     }
 
 }
+
+*/
+
+
 
 
 
