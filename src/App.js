ff83f0a59d6bab1805fe3734b96a4dd4a76cb849
nick <nick.schneider@mmtn-schneider.com>
2023-10-29 00:26:00 +0200

    Fixed reload 404-error with Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 
-import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Login from './pages/Login'
 import PersistLogin from './pages/PersistLogin'
