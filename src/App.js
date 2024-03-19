1d390d322b9e7bd01456783ee455dcc974687724
nick <nick.schneider@mmtn-schneider.com>
2024-03-19 20:40:44 +0100

    Updated OptionsDialog

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import PersistLogin from './logic/PersistLogin'
 import Registration from './pages/Registration'
 
 import CreateGame from './pages/CreateGame'
-import EnterNames from './pages/EnterNames'
 
 import SelectSession from './pages/SelectSession'
 import SessionPreview from './pages/SessionPreview'
@@ -20,32 +19,11 @@ import Game from './pages/Game'
 import EndScreen from './pages/EndScreen'
 
 
-function App() {
-
-	const showOptions = () => {	document.getElementById('modal-options').showModal()}
-
-
-
-
+export default function App() {
 
 	return (
 		<div style={{ display: 'flex', alignItems: 'center', padding: '150px' }}>
-			<div className="App" id='App'>
-
-				<svg 
-					className='button-responsive' 
-					style={{ 
-						position: 'fixed', 
-						top: '5px', 
-						left: '5px' 
-					}} 
-					onClick={showOptions} 
-					height='30' 
-					viewBox='0 -960 960 960' 
-				>
-					<path d='m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z'/>
-				</svg>
-
+			<div className='App' id='App'>
 				<Router>
 					<Routes>
 						{/* 'Public' routes --> routes that can be accessed without token */}
@@ -61,19 +39,18 @@ function App() {
 						*/}
 						<Route element={<PersistLogin />}>
 
-							<Route path='/CreateGame' element={<CreateGame />} />
-							<Route path='/EnterNames' element={<EnterNames />} />
+							<Route path='/creategame' element={<CreateGame />} />
 							
-							<Route path='/SelectSession' element={<SelectSession />} />
-							<Route path='/SessionPreview' element={<SessionPreview />} />
-							<Route path='/SessionPreview/Table' element={<SessionPreviewTable />} />
+							<Route path='/selectsession' element={<SelectSession />} />
+							<Route path='/sessionpreview' element={<SessionPreview />} />
+							<Route path='/sessionpreview/table' element={<SessionPreviewTable />} />
 
-							<Route path='/Game' element={<Game />} />
-							<Route path='/EndScreen' element={<EndScreen />} />
+							<Route path='/game' element={<Game />} />
+							<Route path='/endscreen' element={<EndScreen />} />
 							
 						</Route>
 
-						<Route path='*' element={<CreateGame />} />
+						<Route path='*' element={<SelectSession />} />
 
 					</Routes>
 				</Router>
@@ -82,5 +59,3 @@ function App() {
 	)
 
 }
-
-export default App
