657423b89a5a63beda5d3431c68e442c5f780c64
Nick Leon Schneider <nick.schneider@mmtn-schneider.com>
2025-02-16 09:51:56 +0100

    Fixed IsBorderVisible

--- src/components/Statistics/Chart_Graph.jsx
+++ src/components/Statistics/Chart_Graph.jsx
@@ -17,12 +17,33 @@ export default function Chart_Graph({
 	Data, 
 }) {
 
-	// const [ labels, setLabels ] = useState()
 	const [ values, setValues ] = useState()
 
 
 
 
+
+	// Makes extremely light colors black
+	const adjustColor = (hex) => {
+	
+		if (!hex.startsWith("#") || (hex.length !== 4 && hex.length !== 7)) return hex
+	
+		let r, g, b
+		
+		if (hex.length === 4) {
+			r = parseInt(hex[1] + hex[1], 16);
+			g = parseInt(hex[2] + hex[2], 16);
+			b = parseInt(hex[3] + hex[3], 16);
+		} else {
+			r = parseInt(hex.slice(1, 3), 16);
+			g = parseInt(hex.slice(3, 5), 16);
+			b = parseInt(hex.slice(5, 7), 16);
+		}
+
+		const luminance = 0.299 * r + 0.587 * g + 0.114 * b
+		return luminance > 230 ? 'rgb(100, 100, 100)' : hex
+
+	}
 
 	useEffect(() => {
 
@@ -45,22 +66,20 @@ export default function Chart_Graph({
 	}, [ List_Players, Data ])
 
 
+
+
+
 	if(!Data) return <></>
 	return <>
 		<Line
 			data={{
 				labels, 
 				datasets: List_Players?.map(player => ({
+					backgroundColor: IsBorderVisible ? adjustColor(player.Color) : player.Color, 
+					borderColor: IsBorderVisible ? adjustColor(player.Color) : player.Color, 
 					data: values?.map(value => value[player.id]), 
-					// data: Object.keys(Data).map(key => Data[key].Wins[player.id] || 0), 
-					backgroundColor: player.Color, 
-					borderColor: player.Color, 
 					label: player.Name, 
 					tension: .2,
-					// borderWidth: IsBorderVisible ? 1 : 0,
-					// borderColor: IsBorderVisible ? 'black' : '', 
-					// backgroundColor: 'transparent',
-					// tension: 0.3,
 				}))
 			}}
 			options={{ scales: { y: { beginAtZero: true } } }}
